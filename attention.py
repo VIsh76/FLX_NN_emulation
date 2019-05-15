97b007f95ecea9d908d6ada694041288b962f490
VIsh76 <victordecalignon@gmail.com>
2019-05-15 13:35:27 -0600

    Cleaning jupyter

--- attention.py
+++ attention.py
@@ -1,53 +1,122 @@
-import psutil
-import numpy as np
+import tensorflow as tf
 import os
-import pandas as pd
-import matplotlib.pyplot as plt
-from generator import Basic_Generator
-from preprocess import Zero_One, Normalizer, DictPrepross
-# Contain utilitary functions
-
-def print_ram_usage():
-    process = psutil.Process(os.getpid())
-    ram_usage = round(process.memory_info().rss/float(2**30), 2)
-    print("RAM usage: {}GB".format(ram_usage))
+from tensorflow.python.keras.layers import Layer
+from tensorflow.python.keras import backend as K
 
 
-def Plot_Batch(x0, y0, header):
-    f=plt.figure( figsize=(15,10), dpi=80)
-    for i in range(11):
-        ax= f.add_subplot(3,4,i+1)
-        ax.set_title(header[i])
-        for b in range(x0.shape[0]):
-            ax.plot(np.flip(x0[b,i,:]), np.arange(len(x0[b,i,:])))
-    ax= f.add_subplot(3,4,12)
-    ax.set_title('flx')
-    for b in range(y0.shape[0]):
-        ax.plot(np.flip(y0[b]), np.arange(len(y0[b])))
+class AttentionLayer(Layer):
+    """
+    This class implements Bahdanau attention (https://arxiv.org/pdf/1409.0473.pdf).
+    There are three sets of weights introduced W_a, U_a, and V_a
+     """
 
+    def __init__(self, **kwargs):
+        super(AttentionLayer, self).__init__(**kwargs)
 
+    def build(self, input_shape):
+        assert isinstance(input_shape, list)
+        # Create a trainable weight variable for this layer.
 
+        self.W_a = self.add_weight(name='W_a',
+                                   shape=tf.TensorShape((input_shape[0][2], input_shape[0][2])),
+                                   initializer='uniform',
+                                   trainable=True)
+        self.U_a = self.add_weight(name='U_a',
+                                   shape=tf.TensorShape((input_shape[1][2], input_shape[0][2])),
+                                   initializer='uniform',
+                                   trainable=True)
+        self.V_a = self.add_weight(name='V_a',
+                                   shape=tf.TensorShape((input_shape[0][2], 1)),
+                                   initializer='uniform',
+                                   trainable=True)
 
-########## GET DICTIONNARY :
-fct = []
-for i in range(5):
-    fct.append(Zero_One())
-for j in range(5):
-    fct.append(Normalizer())
-hd = ['rl', 'ri', 'ql', 'qi', 'q', 'ts', 't', 'emis', 'o3', 'pl']
+        super(AttentionLayer, self).build(input_shape)  # Be sure to call this at the end
 
-def Load_FLX_dict(header_dict = hd , path='DictPreprocess_fit.hdf5' , fct=fct):
-    if os.path.isfile(path):
-        Dhd = pd.read_hdf(path, key='s')
-        D = DictPrepross([], [])
-        D.load_from_pd(Dhd)
-    else:
-        print("Fitting Dict")
-        B = Basic_Generator(data_folder)
-        xdim, ydim = B.Xdim, B.Ydim
-        B = Basic_Generator(data_folder, batch_size=xdim*ydim, shuffle=False)
-        D = DictPrepross(header_dict, fct)
-        D.fitonGen(B)
-        Dhd = D.to_array_save()
-        Dhd.to_hdf(path, key='s')
-    return(D)
+    def call(self, inputs, verbose=False):
+        """
+        inputs: [encoder_output_sequence, decoder_output_sequence]
+        """
+        assert type(inputs) == list
+        encoder_out_seq, decoder_out_seq = inputs
+        if verbose:
+            print('encoder_out_seq>', encoder_out_seq.shape)
+            print('decoder_out_seq>', decoder_out_seq.shape)
+
+        def energy_step(inputs, states):
+            """ Step function for computing energy for a single decoder state """
+
+            assert_msg = "States must be a list. However states {} is of type {}".format(states, type(states))
+            assert isinstance(states, list) or isinstance(states, tuple), assert_msg
+
+            """ Some parameters required for shaping tensors"""
+            en_seq_len, en_hidden = encoder_out_seq.shape[1], encoder_out_seq.shape[2]
+            de_hidden = inputs.shape[-1]
+
+            """ Computing S.Wa where S=[s0, s1, ..., si]"""
+            # <= batch_size*en_seq_len, latent_dim
+            reshaped_enc_outputs = K.reshape(encoder_out_seq, (-1, en_hidden))
+            # <= batch_size*en_seq_len, latent_dim
+            W_a_dot_s = K.reshape(K.dot(reshaped_enc_outputs, self.W_a), (-1, en_seq_len, en_hidden))
+            if verbose:
+                print('wa.s>',W_a_dot_s.shape)
+
+            """ Computing hj.Ua """
+            U_a_dot_h = K.expand_dims(K.dot(inputs, self.U_a), 1)  # <= batch_size, 1, latent_dim
+            if verbose:
+                print('Ua.h>',U_a_dot_h.shape)
+
+            """ tanh(S.Wa + hj.Ua) """
+            # <= batch_size*en_seq_len, latent_dim
+            reshaped_Ws_plus_Uh = K.tanh(K.reshape(W_a_dot_s + U_a_dot_h, (-1, en_hidden)))
+            if verbose:
+                print('Ws+Uh>', reshaped_Ws_plus_Uh.shape)
+
+            """ softmax(va.tanh(S.Wa + hj.Ua)) """
+            # <= batch_size, en_seq_len
+            e_i = K.reshape(K.dot(reshaped_Ws_plus_Uh, self.V_a), (-1, en_seq_len))
+            # <= batch_size, en_seq_len
+            e_i = K.softmax(e_i)
+
+            if verbose:
+                print('ei>', e_i.shape)
+
+            return e_i, [e_i]
+
+        def context_step(inputs, states):
+            """ Step function for computing ci using ei """
+            # <= batch_size, hidden_size
+            c_i = K.sum(encoder_out_seq * K.expand_dims(inputs, -1), axis=1)
+            if verbose:
+                print('ci>', c_i.shape)
+            return c_i, [c_i]
+
+        def create_inital_state(inputs, hidden_size):
+            # We are not using initial states, but need to pass something to K.rnn funciton
+            fake_state = K.zeros_like(inputs)  # <= (batch_size, enc_seq_len, latent_dim
+            fake_state = K.sum(fake_state, axis=[1, 2])  # <= (batch_size)
+            fake_state = K.expand_dims(fake_state)  # <= (batch_size, 1)
+            fake_state = K.tile(fake_state, [1, hidden_size])  # <= (batch_size, latent_dim
+            return fake_state
+
+        fake_state_c = create_inital_state(encoder_out_seq, encoder_out_seq.shape[-1])
+        fake_state_e = create_inital_state(encoder_out_seq, encoder_out_seq.shape[1])  # <= (batch_size, enc_seq_len, latent_dim
+
+        """ Computing energy outputs """
+        # e_outputs => (batch_size, de_seq_len, en_seq_len)
+        last_out, e_outputs, _ = K.rnn(
+            energy_step, decoder_out_seq, [fake_state_e],
+        )
+
+        """ Computing context vectors """
+        last_out, c_outputs, _ = K.rnn(
+            context_step, e_outputs, [fake_state_c],
+        )
+
+        return c_outputs, e_outputs
+
+    def compute_output_shape(self, input_shape):
+        """ Outputs produced by the layer """
+        return [
+            tf.TensorShape((input_shape[1][0], input_shape[1][1], input_shape[1][2])),
+            tf.TensorShape((input_shape[1][0], input_shape[1][1], input_shape[0][1]))
+        ]
